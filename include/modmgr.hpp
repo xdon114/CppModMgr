6d14cc2f4418a360ab81eea828f17b1587409c88
xdon114 <linyubin8768@163.com>
2025-05-23 19:04:17 +0800

    Update and rename modmgr.h to modmgr.hpp

--- include/modmgr.hpp
+++ include/modmgr.hpp
@@ -1,5 +1,5 @@
-#ifndef MODMGR_H
-#define MODMGR_H
+#ifndef MODMGR_HPP
+#define MODMGR_HPP
 
 #define MODMGR_VER 0x0105L
 #define MOD_BEGIN(Module) namespace modmgr_ns::Module {
@@ -7,4 +7,4 @@
 #define MOD_SCOPE(Module) modmgr_ns::Module
 #define MOD_IMPORT(Module) using namespace MOD_SCOPE(Module)
 
-#endif // #ifndef MODMGR_H
+#endif // #ifndef MODMGR_HPP
