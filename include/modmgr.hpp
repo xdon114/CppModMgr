497a446ff47fcd3e44aa1ec856e50567299f9d80
xdon114 <linyubin8768@163.com>
2025-06-21 16:28:44 +0800

    Update modmgr.hpp to 1.2

--- include/modmgr.hpp
+++ include/modmgr.hpp
@@ -1,17 +1,13 @@
-#ifndef MODMGR_HPP
-#define MODMGR_HPP
+#ifndef CPPMODMGR_MODMGR_HPP
+#define CPPMODMGR_MODMGR_HPP
 
-#define MODMGR_VER 0x000101L
-#define NS_BEG(NS) namespace NS {
-#define NS_END(NS) }
-#define MOD_BEG(MOD) NS_BEG(modmgr_ns); NS_BEG(CONT__##MOD); NS_BEG(MOD);
-#define MOD_END(MOD) NS_END(MOD); NS_END(CONT__##MOD); NS_END(modmgr_ns); 
-#define DET_BEG(MOD) MOD_BEG(MOD); NS_BEG(_det);
-#define DET_END(MOD) NS_END(_det); MOD_END(MOD);
-#define _MOD_CONT_NS(MOD) modmgr_ns::CONT__##MOD
-#define _MOD_NS(MOD) _MOD_CONT_NS(MOD)::MOD
-#define MOD_USE(MOD) using namespace _MOD_CONT_NS(MOD)
-#define MOD_USE_ALL(MOD) using namespace _MOD_NS(MOD)
-#define DET_USE() using namespace _det;
+#define _MOD_BEGIN(M) 			namespace modmgr_ns {
+#define _MOD_END(M)				}
+#define _MOD_NS(M) 				::modmgr_ns::M
+#define MOD_BEGIN(M) 			_MOD_BEGIN(M); namespace M {
+#define MOD_END(M) 				} _MOD_END(M);
+#define USE_MOD(M) 				namespace M = _MOD_NS(M)
+#define ALL_FROM_MOD(M) 		using namespace _MOD_NS(M)
+#define FROM_MOD(M)				using _MOD_NS(M)::
 
-#endif // #ifndef MODMGR_HPP
+#endif // fprotect
